222298e907e686b748ec7676a25bf83cec644b36
Jellonaa <t2puja00@students.oamk.fi>
2024-10-10 16:06:33 +0300

    init

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, TextInput, View, Text } from 'react-native'
+import { useReducer, useState } from 'react'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return [...state, {text: action.task}]
+    case "REMOVE":
+      return state.filter((todo, index) => index !== action.index)
+    default:
+      return state
+  }
+}
 
 export default function App() {
+  const [task, setTask] = useState('')
+  const [todos, dispatch] = useReducer(reducer, [])
+
+  const renderItem = ({item, index}) => {
+    return (
+      <Text
+        key={index}
+        onPress={() => handleRemoveTodo(index)}
+        style={styles.row}
+      >
+        {item.text}
+      </Text>
+    )
+  }
+
+  const handleAddTodo = (task) => {
+    if (task !== '') {
+      dispatch({ type: 'ADD', task })
+    }
+  }
+
+  const handleRemoveTodo = (index) => {
+    dispatch({ type: 'REMOVE', index })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.add}>
+        <TextInput placeholder="Add new..." value={task} onChangeText={text => setTask(text)} />
+        <Button title="Save" onPress={() => handleAddTodo(task)} />
+      </View>
+      <FlatList data={todos} renderItem={renderItem} />
     </View>
   );
 }
@@ -16,5 +55,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  add: {
+    flexDirection: 'row',
+    backgroundColor: '#ddd',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 32
+  },
+  row: {
+    padding: 8
   },
 });
